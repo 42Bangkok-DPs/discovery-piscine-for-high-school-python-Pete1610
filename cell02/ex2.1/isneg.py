645d80ae4ab63e3fbb01e423be119743c423e3f6
Pete1610 <rapeephat.peet@gmail.com>
2024-10-10 11:57:16 +0700

    Add files via upload

---
+++ cell02/ex2.1/isneg.py
@@ -0,0 +1,8 @@
+number = input("Input your number: ")
+if int(number) > 0:
+    print("this number is positive")
+elif int(number) < 0:
+    print("This number is negative")
+elif int(number) == 0:
+    print("This number is both positive and negative")
+
