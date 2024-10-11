b9eb9f67156e3e58f1bfb76c88429564d654f2ca
Pete1610 <rapeephat.peet@gmail.com>
2024-10-11 12:25:45 +0700

    Add files via upload

---
+++ cell04/ex4.2/calculator.py
@@ -0,0 +1,14 @@
+# Get the first number
+first_number = float(input("Give me the first number: "))
+
+# Get the second number
+second_number = float(input("Give me the second number: "))
+
+# Thank the user
+print("Thank you!")
+
+# Perform calculations and display results
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} / {second_number} = {first_number / second_number}")
+print(f"{first_number} * {second_number} = {first_number * second_number}")
