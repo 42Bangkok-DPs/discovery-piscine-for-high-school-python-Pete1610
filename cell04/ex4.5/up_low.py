826f538f2c4d916843fe150428776fb773198764
Pete1610 <rapeephat.peet@gmail.com>
2024-10-11 12:50:46 +0700

    Add files via upload

---
+++ cell04/ex4.5/up_low.py
@@ -0,0 +1,8 @@
+# Ask the user for a string
+user_input = input("Give me a string: ")
+
+# Swap case of the string
+swapped_string = user_input.swapcase()
+
+# Display the modified string
+print(swapped_string)
