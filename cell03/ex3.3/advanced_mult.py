b8af374c8c571296748d2c6a65ec458f0a85b0cf
Pete1610 <rapeephat.peet@gmail.com>
2024-10-11 11:52:44 +0700

    Add files via upload

---
+++ cell03/ex3.3/advanced_mult.py
@@ -0,0 +1,10 @@
+i = 0
+
+while i <= 10:
+    print(f"Table de {i}:", end=" ")
+    j = 0
+    while j <= 10:
+        print(i * j, end=" ")
+        j += 1
+    print()  # Move to the next line after each table
+    i += 1
